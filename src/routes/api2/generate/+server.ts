a1ede1abf2dbf75e6261f17c4491a5183bd51785
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2025-03-11 03:46:21 -0700

    ok

--- src/routes/api2/generate/+server.ts
+++ src/routes/api2/generate/+server.ts
@@ -1,10 +1,29 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
-
+import { Ollama } from "ollama";
 
 export const GET: RequestHandler = async () => {
-    return json(["hello","world"]);
+  const ollama = new Ollama({ host: "http://localhost:11434/" });
+
+  const dataofMasterUser = {
+    name: "Roescen",
+    course: "BSCS",
+    hobbies: ["Draw 2d arts","Play cozy games"],
   };
 
-  export const POSt: RequestHandler = async () => {
-    return json(["hello","world","from","post"]);
-  };
+  const chat = await ollama.chat({
+    model: "deepseek-r1:1.5b",
+    messages: [
+      {
+        role: "system",
+        content: `Here is the information of your master user: ${JSON.stringify(dataofMasterUser)}
+        Respond only based on data of the master user.`,
+      },
+      {
+        role: "user",
+        content: "Who is your master user?",
+      },
+    ],
+  });
+
+  return json(chat);
+};
