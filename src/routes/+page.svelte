ced54cb981fa08bf23a48fef50ef6937fff72987
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2025-03-13 03:27:33 -0700

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,20 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import MarkdownIt from "markdown-it";
-  
-    let response = "";
-    let md = new MarkdownIt();
-  
-    onMount(async () => {
-      try {
-        const request = await fetch("http://localhost:5173/api2/generate");
-        const apiResponse = await request.json();
-        response = apiResponse?.message?.content || "";
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    });
+  import { onMount } from 'svelte';
+  import MarkdownIt from 'markdown-it';
+
+  let response = $state("")
+  const md = new MarkdownIt();
+
+  onMount(async () => {
+    const request = await fetch("https://localhost:5173/api2/generate")
+      const apiResponse = await request.json ();
+      response = apiResponse?.message?.content || ""
+  });
   </script>
-  
-  <div class="p-4">
-    <div class="rounded p-4 bg-blue-200 shadow">
-      {@html md.render(response)}
+
+<div class="p-4">
+    <div class = "rounded p-4 bg-blue-200 shadow-md">
+        {@html md.render(response)}
+
     </div>
-  </div>
-  
+</div>
--- src/routes/api2/generate/+server.ts
+++ src/routes/api2/generate/+server.ts
@@ -7,27 +7,26 @@ export const GET: RequestHandler = async () => {
   const dataofMasterUser = {
     name: "Roescen",
     course: "BSCS",
-    hobbies: ["Draw 2d arts","Play cozy games"],
+    hobbies: ["Draw 2D arts", "Play cozy games"],
     favoriteColor: "Black",
     age: 22,
-    gender: "Female",
     userTypes: "Master User",
   };
+      const chat = await ollama.chat({
+        model: "deepseek-r1:1.5b",
+        messages: [
+          {
+            role: "system",
+            content: `Here is the information of your master user: ${JSON.stringify(dataofMasterUser)} 
+            Respond only based on the master user's data.`,
+          },
+          {
+            role: "user",
+            content: "Who is your master user?",
+          },
+        ],
+      });
 
-  const chat = await ollama.chat({
-    model: "deepseek-r1:1.5b",
-    messages: [
-      {
-        role: "system",
-        content: `Here is the information of your master user: ${JSON.stringify(dataofMasterUser)}
-        Respond only based on data of the master user.`,
-      },
-      {
-        role: "user",
-        content: "Who is your master user?",
-      },
-    ],
-  });
+      return json(chat)
 
-  return json(chat);
-};
+    };
